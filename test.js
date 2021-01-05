279a9749cad761e3bf40a180d18b9f85a265c750
edwardmavarez <edwardmavarezdev@gmail.com>
2021-01-05 18:59:20 -0400

    put button,correction functions and functional declare focus element

--- test.js
+++ test.js
@@ -1,66 +1,77 @@
-var inside = document.getElementById("space");
-var functButtons = []
 
-for(i=1; i<13; i++){
+var inside = document.getElementById("space");
+var width = document.getElementById("width");
+width.addEventListener("click", changeWidth);
+var widthpx = document.getElementById("widthpx");
+var bodydiv = document.getElementById("body");
+
+function changeWidth(){
+
+  inside.style = ("width:100px") 
+  console.log(widthpx.value);
+
+
+}
+//define quantity of buttons to use
+
+for(i=1; i<17; i++){
   inside.innerHTML+="<input type ='button"+i+"' id = 'funct"+i+"'value = 'funct"+i+"'></input>";
-  loop = ("funct"+i);
+ quantityButtons = i;
 }
 
-// loop for not work to declare this, thinking another function
-functButtons.push(document.getElementById("funct1"));
-functButtons.push(document.getElementById("funct2"));
-functButtons.push(document.getElementById("funct3"));
-functButtons.push(document.getElementById("funct4"));
-functButtons.push(document.getElementById("funct5"));
-functButtons.push(document.getElementById("funct6"));
-functButtons.push(document.getElementById("funct7"));
-functButtons.push(document.getElementById("funct8"));
-functButtons.push(document.getElementById("funct9"));
-functButtons.push(document.getElementById("funct10"));
-functButtons.push(document.getElementById("funct11"));
-functButtons.push(document.getElementById("funct12"));
+//define keyCode Capture to use in DOM event
 
-  /*var functButtons[i]=document.getElementById(loop);*/
-
-// keyboard capture, use an functions
-
-var move = {left:37, up: 38, right: 39, down: 40}
-var x=0
+var move = {left:37, right: 39,up: 38, down: 40}
 document.addEventListener( "keydown", show);
-column=3
-row=4
+
+//responsive buttons
+var columns = 4;
+var button = 1;
+
+
+// keyboard capture, use and functions
+
 function show(event){
   key = event.keyCode
 
-  //left
-    if  (key == move.left && x>0){
-      x--;
-      functButtons[x].focus();
-    }else{}
 
-  //up
-    if  (key == move.up && x>parseInt(functButtons.length/row)){
-        x=x-parseInt(functButtons.length/row);
-      functButtons[x].focus()
+    //function Button selected
+        function buttonSelected(){
+          focusNumber = ("funct"+button);
+          var focus = document.getElementById(focusNumber);
+          focus.focus();
+        }
+
+  //left
+    if  (key == move.left && button>1){
+      button--;
+      buttonSelected();
     }
-    else if(key == move.up){
-      x=0;
-      functButtons[x].focus();
-    }
+
 
   //right
-    if  (key == move.right && x < functButtons.length-1){
-      x++;
-      functButtons[x].focus()
-    }else{}
+    if  (key == move.right && button < quantityButtons){
+      button++;
+      buttonSelected();
+    }
+
+    //up
+      if  (key == move.up && button>columns){
+          button = button - columns;
+          buttonSelected();
+      }
+      else if(key == move.up){
+        button=1;
+        buttonSelected();
+      }
 
   //down
-      if  (key == move.down && x < parseInt((functButtons.length/row)*column)){
-      x=x+(functButtons.length/row);
-      functButtons[x].focus()
-    }
-    else if(key == move.down){
-      x=parseInt(functButtons.length-1);
-      functButtons[x].focus();
+  if  (key == move.down && button<(quantityButtons-columns)){
+      button = button + columns;
+      buttonSelected();
+  }
+  else if(key == move.down){
+    button=quantityButtons;
+    buttonSelected()
   }
 }
