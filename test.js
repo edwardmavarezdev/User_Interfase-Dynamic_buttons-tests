0aca3dc05d919552a6c33f84abb79cd64ffdd388
edwardmavarez <edwardmavarezdev@gmail.com>
2021-01-06 17:42:36 -0400

    columns & buttons work, have a limit and is resposive. buttons to modify work

--- test.js
+++ test.js
@@ -1,77 +1,131 @@
 
-var inside = document.getElementById("space");
-var width = document.getElementById("width");
-width.addEventListener("click", changeWidth);
-var widthpx = document.getElementById("widthpx");
-var bodydiv = document.getElementById("body");
+var space = document.getElementById("space");
+var buttonPlace = document.getElementById("buttonPlace");
+var htmlUse = document.getElementById('htmlUse');
 
+var columns = 4
+var createButtons = 16
+
+//testing variables
+var lessC = document.getElementById("lessC");
+lessC.addEventListener("click", lessColumns);
+var moreC = document.getElementById("moreC");
+moreC.addEventListener("click", moreColumns);
+
+var lessB = document.getElementById("lessB");
+lessB.addEventListener("click", lessButtons);
+var moreB = document.getElementById("moreB");
+moreB.addEventListener("click", moreButtons);
+
+function lessColumns(){
+  columns--;
+  declareSizeofSquare();
+  }
+function moreColumns(){
+  columns++;
+  declareSizeofSquare();
+  }
+
+
+function lessButtons(){
+  createButtons--;
+    if (createButtons<1){
+        createButtons++;
+        alert("You can not have cero Buttons");
+      }
+  declareSizeofSquare();
+  }
+function moreButtons(){
+  createButtons++;
+  declareSizeofSquare();
+  }
+//--------------------
+// test changeWidth
 function changeWidth(){
 
-  inside.style = ("width:100px") 
+  inside.style = ("width:"+widthpx.value+"px")
   console.log(widthpx.value);
-
-
 }
-//define quantity of buttons to use
+//-------------------
 
-for(i=1; i<17; i++){
-  inside.innerHTML+="<input type ='button"+i+"' id = 'funct"+i+"'value = 'funct"+i+"'></input>";
- quantityButtons = i;
+//print buttons
+function declareSizeofSquare(){
+space.innerHTML=""
+
+for(i=1; i<(createButtons+1); i++){
+  space.innerHTML+="<input type ='button' id = 'buttonId"+i+"'value = 'button-"+i+"'></input>";
+  quantityButtons = i;
+    }
+    if (columns<1){
+      columns++;
+      alert("You can not have less than 1 column");
+    }
+    if (columns*100>htmlUse.clientWidth){
+      columns--;
+      alert("You can not have more columns than your screen size")
+    }
+    space.style = "width:"+(columns*100)+"px"
 }
+
+//redefine style (not used now)
+function redefineStyle(){
+  buttonPlace.style = "width:"+(columns*100)+"px"
+}
+
+//begin load
+declareSizeofSquare();
 
 //define keyCode Capture to use in DOM event
 
-var move = {left:37, right: 39,up: 38, down: 40}
 document.addEventListener( "keydown", show);
 
 //responsive buttons
-var columns = 4;
-var button = 1;
+var buttonToSelect = 1;
 
 
+var move = {left:37, right: 39,up: 38, down: 40}
 // keyboard capture, use and functions
 
 function show(event){
   key = event.keyCode
 
-
     //function Button selected
         function buttonSelected(){
-          focusNumber = ("funct"+button);
+          focusNumber = ("buttonId"+buttonToSelect);
           var focus = document.getElementById(focusNumber);
           focus.focus();
         }
 
   //left
-    if  (key == move.left && button>1){
-      button--;
+    if  (key == move.left && buttonToSelect>1){
+      buttonToSelect--;
       buttonSelected();
     }
 
 
   //right
-    if  (key == move.right && button < quantityButtons){
-      button++;
+    if  (key == move.right && buttonToSelect < quantityButtons){
+      buttonToSelect++;
       buttonSelected();
     }
 
     //up
-      if  (key == move.up && button>columns){
-          button = button - columns;
+      if  (key == move.up && buttonToSelect>columns){
+          buttonToSelect = buttonToSelect - columns;
           buttonSelected();
       }
       else if(key == move.up){
-        button=1;
+        buttonToSelect=1;
         buttonSelected();
       }
 
   //down
-  if  (key == move.down && button<(quantityButtons-columns)){
-      button = button + columns;
+  if  (key == move.down && buttonToSelect<(quantityButtons-columns)){
+      buttonToSelect = buttonToSelect + columns;
       buttonSelected();
   }
   else if(key == move.down){
-    button=quantityButtons;
+    buttonToSelect=quantityButtons;
     buttonSelected()
   }
 }
